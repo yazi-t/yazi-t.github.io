1a438bd4ac9bcc53dbf0d5da1110f7c0ee0b10aa
yazi-t <yasitha009@gmail.com>
2019-10-26 11:32:33 +0530

    New modified web

--- js/main.js
+++ js/main.js
@@ -8,5 +8,37 @@ jQuery(document).ready(function($) {
             easing: 'snap',
             transitionSpeed: 400
         });
+		
+		$("#form-submit").click(function(){
+			window.open('mailto:' + document.getElementById("email").value 
+			+ '?subject=Github visitor - ' + document.getElementById("name").value 
+			+ '&body=' + document.getElementById("message").value + '');
+		});
+		
+		var dt = new Date();
+		var yr = dt.getFullYear();
+		
+		var i = 1995;
+		$('#cp-year').html(i);
+		
+		var scrolledBottom = false;
+		$(window).scroll(function (event) {
+			var scroll = $(window).scrollTop();
+			if (!scrolledBottom && scroll > position) {
+				incrementYear();
+				scrolledBottom = true;
+			}
+		});
+		
+		function incrementYear() {
+			i++;
+			$('#cp-year').html(i);
+			console.log("val: " + i);
+			if (i < yr) {
+				setTimeout(function() {
+					incrementYear();
+				}, 50);
+			}
+		}
 
 });
